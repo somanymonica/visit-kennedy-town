59719ef916ca8759b42c44e09ee73bc3fe00e1db
monica <lishutingmm@gmail.com>
2023-03-01 20:05:22 +0800

    deploy config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // basePath: "/kennedytown",
+
   output: "standalone",
+  assetPrefix: "kennedytown", //是用docker-compose和nginx部署时才用的
+  async rewrites() {
+    // 看起来没有用到
+    return [
+      {
+        source: "/about",
+        destination: "/kennedytown/about",
+      },
+    ];
+  },
 };
