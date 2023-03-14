ad2b056a030d42d548544d1a6d538c9ca95f2ded
monica <lishutingmm@gmail.com>
2023-03-15 00:13:24 +0800

    sass: header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,25 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
-import images from "../constants/images";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 
 const Header = () => (
-  <div className="section__container" id="home">
-    <div className={styles.header_img}>
-      <Image src={images.kd} alt="header" className={styles.header_img_img} />
-    </div>
-    <div className={styles.header_info}>
-      <h1 className="header_h1">Welcome to Kennedy Town</h1>
-      <p className="header_p">
-        Kennedy Town is at the western end of Sai Wan on Hong Kong Island in
-        Hong Kong.
-        <br /> I love Kennedy Town and hope you do too.
-      </p>
-      <button className="custom_button">
-        <Link href="/posts">Explore More</Link>
-      </button>
-    </div>
+  <div className={styles.header} id="home">
+    <h1>Welcome to Kennedy Town</h1>
   </div>
 );
 
