925c674fbb76ec462cbae6586b16533479b9e039
monica <lishutingmm@gmail.com>
2023-03-15 13:05:59 +0800

    sass: index-scenes

--- client/components/Scenes.jsx
+++ client/components/Scenes.jsx
@@ -1,36 +1,32 @@
-import styles from "./Scenes.module.css";
+import styles from "./Scenes.module.scss";
 import Image from "next/image";
 
-import { images } from "../constants";
 import { data } from "../constants";
 
 const RecommandCard = ({ imgUrl, title, desc }) => (
   <div className={styles.recommand_card}>
     <Image src={imgUrl} alt="recommand-card" />
     <div className={styles.recommand_card_info}>
-      <h3 className="info_h3">{title}</h3>
-      <p className="info_p">{desc}</p>
+      <h3>{title}</h3>
+      <p>{desc}</p>
     </div>
   </div>
 );
 
 const Scenes = () => (
-  <div className="section__container" id="scenes">
-    <div className={styles.scenes_info}>
-      <h1 className="header_h1">Recommand Places</h1>
-      <div className={styles.scenes_info_container}>
-        {data.places.map((place, index) => (
-          <RecommandCard
-            key={place.title + index}
-            imgUrl={place.imgUrl}
-            title={place.title}
-            desc={place.desc}
-          ></RecommandCard>
-        ))}
-      </div>
+  <div className={styles.scenes} id="scenes">
+    <div className={styles.scenes_header}>
+      <h1>Beautiful Scenes</h1>
     </div>
-    <div className={styles.scenes_cover}>
-      <Image src={images.s} alt="scenes-cover" />
+    <div className={styles.scenes_info_container}>
+      {data.places.map((place, index) => (
+        <RecommandCard
+          key={place.title + index}
+          imgUrl={place.imgUrl}
+          title={place.title}
+          desc={place.desc}
+        ></RecommandCard>
+      ))}
     </div>
   </div>
 );
